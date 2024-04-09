4dbd520dc27f6a1d48d6b3f39566900de6b012ce
Kirill Ubushaev <ubushaevkirill@gmail.com>
2024-04-09 19:53:41 +0300

    fix bug

--- src/widgets/PostList/ui/PostList.tsx
+++ src/widgets/PostList/ui/PostList.tsx
@@ -1,35 +1,19 @@
-import { Container, List } from "@mui/material"
 import { PostApi } from "../../../entities/PostItem"
 import { PostItem } from "../../../entities/PostItem"
-import { useEffect, useState } from "react"
+import { useCallback, useState } from "react"
+import { Virtuoso } from "react-virtuoso"
 
 export function PostList() {
 
     const [page, setPage] = useState(1)
 
-    const { data: posts, isLoading, isFetching } = PostApi.useFetchAllPostsQuery(page)
+    const { data: posts, isLoading } = PostApi.useFetchAllPostsQuery(page)
 
-    useEffect(() => {
-        const onScroll = () => {
-            const scrolledToBottom =
-                window.innerHeight + window.scrollY >= document.body.offsetHeight;
-            const scrolledToTop = window.scrollY === 0;
-
-            if (scrolledToBottom && !isFetching) {
-                setPage((prevPage) => prevPage + 1);
-            }
-
-            if (scrolledToTop && !isFetching && page > 1) {
-                setPage((prevPage) => prevPage - 1);
-            }
-        };
-
-        document.addEventListener("scroll", onScroll);
-
-        return function () {
-            document.removeEventListener("scroll", onScroll);
-        };
-    }, [isFetching]);
+    const loadMore = useCallback(() => {
+        return setTimeout(() => {
+            setPage((prevPage) => prevPage + 1)
+        }, 500)
+    }, [setPage])
 
     if (isLoading) {
         return <div>Loading</div>
@@ -40,19 +24,16 @@ export function PostList() {
     }
 
     return (
-        <Container>
-            <List sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                gap: '1em',
-                mb: 2
-            }}>
-                {posts && !isLoading && posts.map(post =>
-                    <PostItem key={post.id} post={post} />
-                )}
-            </List>
-        </Container>
+        <Virtuoso
+            useWindowScroll
+            data={posts}
+            endReached={loadMore}
+            increaseViewportBy={200}
+            itemContent={(index, post) => {
+                return <PostItem key={post.id} post={post} />
+            }}
+
+        />
     )
 }
 
